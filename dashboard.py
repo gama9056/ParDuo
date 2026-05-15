9ff8ff7208e432500c1b7ed29dfa9e9a6f2360d1
gama9056 <gama9056@gmail.com>
2026-05-14 23:10:38 -0500

    Adjust header margin in dashboard.py

--- dashboard.py
+++ dashboard.py
@@ -31,7 +31,7 @@ def cargar_dashboard():
 
 # ========== TÍTULO ==========
 st.markdown("""
-    <h1 style="color: black; margin-top: -60px; text-align: center;">😎 ParDuo</h1>
+    <h1 style="color: black; margin-top: -55px; text-align: center;">😎 ParDuo</h1>
 """, unsafe_allow_html=True)
 
 # ========== CARGAR DATOS ==========
