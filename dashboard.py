847023c936c449e78d79d081576c4381cb0e066a
gama9056 <gama9056@gmail.com>
2026-05-15 00:40:25 -0500

    Update dashboard.py

--- dashboard.py
+++ dashboard.py
@@ -36,7 +36,7 @@ st.markdown("""
                text-align: center; 
                font-size: 30px; 
                font-family: 'Segoe UI'; 
-               text-shadow: 2px 2px 4px rgba(0,0,0,0.3); 
+               text-shadow: 3px 3px 5px rgba(0,0,0,0.4); 
                letter-spacing: 1px">
         😎 ParDuo
     </h1>
